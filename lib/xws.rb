737195654b3f5b7cf8c4b9fdcbeb1e1785997df2
Your Name <you@example.com>
2015-11-13 13:44:36 +0000

    bug fix: non-words should now only be rejected and not any of the remaining words after a non-word.

--- lib/xws.rb
+++ lib/xws.rb
@@ -14,11 +14,12 @@ class XWS
   end
 
   def scan(node)
-
+    puts 'inside scan'
+    puts 'node : ' + node.xml.inspect
     a = []
 
     node.each_recursive do |x|
-
+      puts 'x : ' + x.inspect
       if not x.name[/#{@ignore_elements.join('|')}/] then
         a += x.texts.map(&:strip).select{|x| not x.empty?}
       end
@@ -39,7 +40,7 @@ class XWS
       #             non-alpabetical characters from start or beginning of words
       gsub(/\s.\s/,' ').                              # remove single digits 
       gsub(/\b(?:#{@ignorewords.join('|')})\b/,'').   # ignore common words
-      gsub(/\B[^\w#].*/,'')              # remove any other items which are 
+      gsub(/\B[^\w#]\B+/,'')              # remove any other items which are 
       #                                 not words or hashtags
     
   end
--- xws.gemspec
+++ xws.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'xws'
-  s.version = '0.1.3'
+  s.version = '0.1.4'
   s.summary = 'XML Word Scanner (XWS) reads a Rexle document and returns a hash object containing each word found.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/xws.rb']
